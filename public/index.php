1b95107d88587f768a89dc2f272a496e2a7ffb6b
eric hoppenworth <eric.hoppenworth@gmail.com>
2018-02-18 21:42:14 -0500

    jaws added

--- public/index.php
+++ public/index.php
@@ -3,7 +3,12 @@
 $request_uri = explode('?', $_SERVER['REQUEST_URI'], 2);
 
 if(getenv('JAWSDB_URL')){
-    $mysqli = new mysqli(getenv('JAWSDB_URL'));
+    $mysqli = new mysqli(
+        'l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        'bjsitggtb4yob6ak',
+        'ygg1bb39c1sm6544',
+        'yjukqyl7lpuo363c'
+    );
 } else {
     $mysqli = new mysqli("localhost", "root", "Rice3773", "bamazon");
 }
